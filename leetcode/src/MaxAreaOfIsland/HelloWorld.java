907aa234d37c20c34d754d808d0532f3e5991c76
alejandroserrano <u1214728@utah.edu>
2022-12-22 21:41:27 -0500

    clone graph

--- leetcode/src/MaxAreaOfIsland/HelloWorld.java
+++ leetcode/src/MaxAreaOfIsland/HelloWorld.java
@@ -3,6 +3,7 @@ package MaxAreaOfIsland;
 public class HelloWorld {
 
     public static void main(String[] args) {
-
+        System.out.println("Hello friends :D"); 
+        System.out.println("Java is cool 😃");
     }
 }
