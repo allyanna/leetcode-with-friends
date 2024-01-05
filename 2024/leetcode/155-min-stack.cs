2ba91540a30d8e47c801abdc8d3aeea5fa02d4b8
Alejandro <50674421+ajserrano9310@users.noreply.github.com>
2024-01-05 09:11:39 -0700

    Update 155-min-stack.cs

--- 2024/leetcode/155-min-stack.cs
+++ 2024/leetcode/155-min-stack.cs
@@ -1,1 +1,45 @@
 
+public class MinStack {
+
+    public Stack<int> stack;
+    public Stack<int> minStack;  
+    public MinStack() {
+        stack = new Stack<int>(); 
+        minStack = new Stack<int>();
+    }
+    
+    public void Push(int val) {
+        stack.Push(val); 
+
+        if(minStack.Count > 0) {
+            int currMin = minStack.Peek();
+
+            if(currMin < val) {
+                minStack.Push(currMin); 
+            }
+            else {
+                minStack.Push(val);
+            }
+        } else {
+            minStack.Push(val);
+        }
+    }
+    
+    public void Pop() {
+        if(stack.Count > 0) {
+            stack.Pop();
+        }
+        if(minStack.Count > 0) {
+            minStack.Pop();
+        }
+         
+    }
+    
+    public int Top() {
+        return stack.Peek();
+    }
+    
+    public int GetMin() {
+        return minStack.Peek();
+    }
+}
