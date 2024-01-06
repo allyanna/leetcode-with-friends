6523689dc7158cd3f2e4c81005f542050bd9535c
Alejandro <50674421+ajserrano9310@users.noreply.github.com>
2024-01-06 06:12:51 -0700

    Create 230-Kth-Smallest-Element-BST.py

---
+++ 2024/leetcode/230-Kth-Smallest-Element-BST.py
@@ -0,0 +1,12 @@
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        res = []
+        def inorder(node):
+            if not node: return
+            inorder(node.left)
+            if len(res) == k:
+                return
+            res.append(node.val)
+            inorder(node.right)
+        inorder(root)
+        return res[-1]
