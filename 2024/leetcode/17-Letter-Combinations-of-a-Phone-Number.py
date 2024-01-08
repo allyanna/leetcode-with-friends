47a4b420c8259d7622fb024a6e9bc280d1b087e4
Alejandro <50674421+ajserrano9310@users.noreply.github.com>
2024-01-08 08:59:21 -0700

    Create 17-Letter-Combinations-of-a-Phone-Number.py

---
+++ 2024/leetcode/17-Letter-Combinations-of-a-Phone-Number.py
@@ -0,0 +1,25 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        def bt(i, curr_str):
+            if len(curr_str) == len(digits):
+                res.append(curr_str)
+                return
+            for c in digitToChar[digits[i]]:
+                bt(i + 1, curr_str + c)
+            
+        if digits:
+            bt(0, "")
+        
+        return res
